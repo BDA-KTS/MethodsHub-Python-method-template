e9c66a70aea76573577b896939055c405769cd50
Taimoor Khan <75726383+taimoorkhan-nlp@users.noreply.github.com>
2026-05-08 17:53:07 +0200

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,1 +1,13 @@
+import subprocess
+import sys
 
+env_manager = "{{ cookiecutter.env_manager }}"
+
+if env_manager == "conda":
+    print("Skipping pip install (conda environment expected).")
+
+elif env_manager == "pip":
+    subprocess.run(
+        [sys.executable, "-m", "pip", "install", "-r", "requirements.txt"],
+        check=True
+    )
