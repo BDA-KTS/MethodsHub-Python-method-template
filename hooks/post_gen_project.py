0fdeb1443a25ded0f12f95f43220ccec27ac74ef
Taimoor Khan <75726383+taimoorkhan-nlp@users.noreply.github.com>
2026-05-20 11:48:57 +0200

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,6 +3,7 @@ import sys
 import os
 import shutil
 from pathlib import Path
+import platform
 
 env_manager = "{{ cookiecutter.env_manager }}"
 method_slug = "{{ cookiecutter.method_slug }}"
@@ -10,42 +11,32 @@ method_slug = "{{ cookiecutter.method_slug }}"
 # ----------------------
 # PIP install from requirements.txt
 # ----------------------
-license_name = "{{ cookiecutter.license }}"
-mapping = {
-    "MIT License": "licenses/MIT.txt",
-    "Apache-2.0 License": "licenses/Apache-2.0.txt",
-    "GPL-3.0 License": "licenses/GPL-3.0.txt"
-    "BSD-2-clause License"
-    "No License"
-}
-
-if license_name != "No License":
-    src = Path.cwd() / "licenses" / "{{ cookiecutter.license }}"
-    dst = Path.cwd() / "LICENSE"
-    shutil.copy(src, dst)
-    shutil.rmtree(Path.cwd() / "licenses")
-    print(f"Added {license_name} license.")
-else:
-    print("You have selected no license option. Please manually add an open license to your method")
-# ----------------------
-# PIP install from requirements.txt
-# ----------------------
 if env_manager == "pip":
     venv_path = Path(".venv").resolve()
 
+    print("Opening new terminal for new virtual environment.")
     if os.name == "nt":
         activate = venv_path / "Scripts" / "activate.bat"
         #subprocess.Popen(f'start cmd.exe /k "{activate}"', shell=True)
 
         cmd = f'call "{activate}" && pip install -r binder/requirements.txt && cmd /k'
         subprocess.Popen(f'start cmd.exe /k "{cmd}"', shell=True)
-        print("dependencies installed in the .venv environment.")
     else:
+        venv_python = venv_path / "bin" / "python"
+        subprocess.run([str(venv_python), "-m", "pip", "install", "-r", "binder/requirements.txt"], check=True)
+
         activate = venv_path / "bin" / "activate"
-        subprocess.Popen(["gnome-terminal","--","bash","-ic",f"source '{activate}' && exec bash"])
-
-    print("Opened new terminal with activated virtual environment.")
-
+        cmd = (f"source '{activate}' && " f"exec bash")
+        
+        if platform.system() == "Linux":
+            subprocess.Popen(["gnome-terminal", "--", "bash", "-ic", cmd])
+        elif system == "Darwin":  # macOS
+            subprocess.Popen(["osascript", "-e", f'tell application "Terminal" to do script "{cmd}"'])
+        #activate = venv_path / "bin" / "activate"
+        #subprocess.Popen(["gnome-terminal","--","bash","-ic",f"source '{activate}' && exec bash"])
+    
+    print("Virtual environment .venv created and existing dependencies from {{ cookiecutter.method_slug }}/binder/requirements.txt are installed.")
+    
     print("Installing dependencies for {{ cookiecutter.method_title }} from requirements.txt...")
     subprocess.run([sys.executable,"-m","pip","install","-r","binder/requirements.txt"],check=True)
 
@@ -74,3 +65,30 @@ elif env_manager == "conda":
     )
 
     print("Conda environment updated.")
+
+# ----------------------
+# Add an open license
+# ----------------------
+license_name = "{{ cookiecutter.license }}"
+mapping = {
+    "MIT License": "licenses/MIT.txt",
+    "Apache-2.0 License": "licenses/Apache-2.0.txt",
+    "GPL-3.0 License": "licenses/GPL-3.0.txt"
+    "BSD-2-clause License"
+    "No License"
+}
+
+if license_name != "No License":
+    src = Path.cwd() / "licenses" / "{{ cookiecutter.license }}"
+    dst = Path.cwd() / "LICENSE"
+    shutil.copy(src, dst)
+    shutil.rmtree(Path.cwd() / "licenses")
+    print(f"Added {license_name} license.")
+else:
+    print("You have selected no license option. Please manually add an open license to your method")
+
+print("Method folder created with the name {{ cookiecutter.method_slug }}, with directory structure and mandatory files.")
+
+print("{{cookiecutter.method_title}} and contact details section are updated on README with the information provided.")
+
+print("{{ cookiecutter.method_title }}, {{ cookiecutter.authors_info }}, and {{ cookiecutter.release_date }} are updated in CITATION.CFF file.")
