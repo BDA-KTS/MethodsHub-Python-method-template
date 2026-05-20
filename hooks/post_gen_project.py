c0ffd57816a64cf60d8c181c95949fc17b723790
Taimoor Khan <75726383+taimoorkhan-nlp@users.noreply.github.com>
2026-05-20 14:47:55 +0200

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -35,7 +35,12 @@ if env_manager == "pip":
         cmd = (
             f'call "{activate}" &&'
             f'pip install -r binder/requirements.txt &&' 
-            f'echo "{{ cookiecutter.method_slug }}"/.venv activated &&'
+            f'echo "{{ cookiecutter.method_slug }}"/.venv created and activated &&'
+            f'echo dependencies from "{{ cookiecutter.method_slug }}"/binder/requirements.txt are installed &&'
+            f'echo "{{ cookiecutter.method_title }}", "{{ cookiecutter.authors_info }}", and "{{ cookiecutter.release_date }}" are updated in "{{ cookiecutter.method_slug }}"/CITATION.CFF file &&'
+            f'echo "{{ cookiecutter.method_title }}" and Contact details section (with "{{ cookiecutter.authors_info }}", "{{ cookiecutter.contact_email }}", and "{{ cookiecutter.github_repository_owner }}") are updated in "{{ cookiecutter.method_slug }}"/README.md &&'
+            f'echo "{license_message}" &&'
+            f'Setup is done, Yay 🎉 \nnow grab a coffee ☕, and develop your method. \nGood Luck &&'
             f'cmd /k'
         )
         subprocess.Popen(f'start cmd.exe /k "{cmd}"', shell=True)
